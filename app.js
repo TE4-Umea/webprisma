94369e476f8582214c21d1f3f5182717799bc029
= <=>
2023-10-10 13:51:38 +0200

    Mer kaos app

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ const cors = require('cors')
 app.use(cors())
  
 app.get('/', async function (req, res) {
-    res.json ({
+
+    await prisma.Items.create ({
+        data: {
+            id : 1,
+            name: 'Item 1',
+            price: 1000,
+            quantity: 10,
+            sizes: 'M'
+        }
+    })  
+
+    await prisma.Users.create ({
+        data: {
+            id : 1,
+            name: 'User 1',
+            email: 'email@email.com',
+            password: 'password',
+            address: 'addrees1',
+            role: 'user',
+        }
     })
 })
 
