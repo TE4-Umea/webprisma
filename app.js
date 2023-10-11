9c6a7aea77b0f6e880f47dd1d33440ce3b69169e
= <=>
2023-10-11 14:37:24 +0200

    funkar inte

--- app.js
+++ app.js
@@ -8,26 +8,6 @@ app.use(cors())
 
 app.get('/', async function (req, res) {
 
-    await prisma.Product.create ({
-        data: {
-            id : 1,
-            name: 'Item 1',
-            price: 1000,
-            quantity: 10,
-            sizes: 'M'
-        }
-    })  
-
-    await prisma.Users.create ({
-        data: {
-            id : 1,
-            name: 'User 1',
-            email: 'email@email.com',
-            password: 'password',
-            address: 'addrees1',
-            role: 'user',
-        }
-    })
 })
 
 app.listen(3000)
--- prisma/seed.js
+++ prisma/seed.js
@@ -3,54 +3,58 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-  const product = await prisma.product.create({
-    data: {
-      name: 'Fortnite Tröja',
-      description: 'Fortnite Tröja',
-      price: 39,
-      quantity: 10,
-      categories: {
-        create: [
-          {
-            category: {
-              connectOrCreate: {
-                create: {
-                  name: 'Man',
-                },
-                where: {
-                  name: 'Man',
-                },
-              },
+    const product = await prisma.product.create({
+        data: {
+            id: 1,
+            name: 'Fortnite Tröja',
+            description: 'Fortnite Tröja',
+            price: 39,
+            quantity: 10,
+            sizes : 'M',
+            categories: {
+                create: [
+                    {
+                        category: {
+                            connectOrCreate: {
+                                create: {
+                                    name: 'Man',
+                                },
+                                where: {
+                                    name: 'Man',
+                                },
+                            },
+                        },
+                    },
+                    {
+                        category: {
+                            connectOrCreate: {
+                                create: {
+                                    name: 'Tröjor',
+                                },
+                                where: {
+                                    name: 'Tröjor',
+                                },
+                            },
+                        },
+                    },
+                ],
             },
-          },
-          {
-            category: {
-              connectOrCreate: {
-                create: {
-                  name: 'Tröjor',
-                },
-                where: {
-                  name: 'Tröjor',
-                },
-              },
-            },
-          },
-        ],
-      },
-    },
-  })
-  console.log({ product })
+        },
+
+    })
+
+    console.log({ product })
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
 
-  .catch(async (e) => {
-    console.error(e)
+    .catch(async (e) => {
+        console.error(e)
 
-    await prisma.$disconnect()
+        await prisma.$disconnect()
 
-    process.exit(1)
-  })
+        process.exit(1)
+    })
