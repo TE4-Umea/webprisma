7c1ef8381f705726bc58bad4348a5e9b95a024cb
= <=>
2023-10-12 13:10:53 +0200

    ea

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,44 +3,46 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-    const product = await prisma.product.create({
-        data: {
-            name: 'Fortnite Tröja',
-            description: 'Fortnite Tröja',
-            price: 39,
-            quantity: 10,
-            sizes : 'M',
-            categories: {
-                create: [
-                    {
-                        category: {
-                            connectOrCreate: {
-                                create: {
-                                    name: 'Man',
-                                },
-                                where: {
-                                    name: 'Man',
-                                },
-                            },
-                        },
-                    },
-                    {
-                        category: {
-                            connectOrCreate: {
-                                create: {
-                                    name: 'Tröjor',
-                                },
-                                where: {
-                                    name: 'Tröjor',
+    for (let i = 0; i < 100; i++) {
+        const product = await prisma.product.create({
+            data: {
+                name: `Product ${i}`,
+                description: 'Fortnite Tröja',
+                price: Math.random() * 1000,
+                quantity: Math.random() * 10,
+                sizes : 'M',
+                categories: {
+                    create: [
+                        {
+                            category: {
+                                connectOrCreate: {
+                                    create: {
+                                        name: 'Man',
+                                    },
+                                    where: {
+                                        name: 'Man',
+                                    },
                                 },
                             },
                         },
-                    },
-                ],
+                        {
+                            category: {
+                                connectOrCreate: {
+                                    create: {
+                                        name: 'Tröjor',
+                                    },
+                                    where: {
+                                        name: 'Tröjor',
+                                    },
+                                },
+                            },
+                        },
+                    ],
+                },
             },
-        },
 
-    })
+        })
+    }
 
     console.log({ product })
 }
